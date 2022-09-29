69130fa0a1f24886e7334d6161d3c23b12e478c7
unknown <nwy98@naver.com>
2022-09-29 23:44:10 +0900

    Typescript added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  //styled를 사용시 CSS작성법으로 사용가능, 원하는 이름 가능
+  display: flex;
+  width: 100vh;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>HELLO</Title>
+    </Wrapper>
   );
 }
 
 export default App;
+//styled.input.attrs({required:true}) 작성시  컴포넌트 생성시 속성 부여 가능
+//animation은 styled가 아닌 keyframes로 작성
